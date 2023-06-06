0fb0739767e1e498f22e35f7b079a6ab1193489f
Abhijit Bhopatkar <bain@crave.io>
2023-06-06 15:00:58 +0530

    Use uname -m to append right arch type while downloading crave on linux

--- get_crave.sh
+++ get_crave.sh
@@ -1,15 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 # Copyright (c) 2020-2023 Crave crave.io Inc. All rights reserved
 
 os='linux'
 crave_url_base='https://github.com/accupara/crave/releases/download/'
 crave_version='0.2-6767'
-crave_postfix='amd64.bin'
-
+crave_arch='amd64'
+crave_postfix='.bin'
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     # Linux
     os='linux'
+    crave_arch=`uname -m`
+    if [[ "$crave_arch" == "arm64" ]]; then
+        crave_arch="aarch64"
+    fi
+    if [[ "$crave_arch" == "x86_64" ]]; then
+        crave_arch="amd64"
+    fi
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     # Mac OSX
     os='darwin'
@@ -23,7 +30,7 @@ if [[ "os" == 'unknown' ]]; then
     exit 1
 fi
 
-crave_url="$crave_url_base/$crave_version/crave-$crave_version-$os-$crave_postfix"
-
+crave_url="$crave_url_base/$crave_version/crave-$crave_version-$os-$crave_arch$crave_postfix"
+echo $crave_url
 curl -L $crave_url --output crave
 chmod +x crave
