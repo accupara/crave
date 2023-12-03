d63810cd33a557239c01537968ff68f2f79e02f1
Yuvraaj Kelkar <uv@crave.io>
2023-12-02 16:19:48 -0800

    update to 6882

--- get_crave.ps1
+++ get_crave.ps1
@@ -3,7 +3,7 @@
 $OS="Windows"
 
 $Env:CRAVE_URL_BASE = "https://github.com/accupara/crave/releases/download/"
-$Env:CRAVE_VERSION = "0.2-6873"
+$Env:CRAVE_VERSION = "0.2-6882"
 $Env:CRAVE_POSTFIX = ".zip"
 if ($IsWindows -or $ENV:OS) {
     Write-Host "Downloading $Env:CRAVE_VERSION for Windows"
