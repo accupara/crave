15a935e32e0432ed5f9315cb80f3ae150e9f32ce
Yuvraaj Kelkar <uv@crave.io>
2024-05-08 16:00:10 -0700

    new version of the client

--- get_crave.ps1
+++ get_crave.ps1
@@ -3,7 +3,7 @@
 $OS="Windows"
 
 $Env:CRAVE_URL_BASE = "https://github.com/accupara/crave/releases/download/"
-$Env:CRAVE_VERSION = "0.2-6956"
+$Env:CRAVE_VERSION = "0.2-6965"
 $Env:CRAVE_POSTFIX = ".zip"
 if ($IsWindows -or $ENV:OS) {
     Write-Host "Downloading $Env:CRAVE_VERSION for Windows"
