9693bb9c94f03d5f371d61cf6254c75b8e9cedd6
Yuvraaj Kelkar <uv@crave.io>
2024-09-26 21:09:10 -0700

    7037

--- get_crave.ps1
+++ get_crave.ps1
@@ -3,7 +3,7 @@
 $OS="Windows"
 
 $Env:CRAVE_URL_BASE = "https://github.com/accupara/crave/releases/download/"
-$Env:CRAVE_VERSION = "0.2-7033"
+$Env:CRAVE_VERSION = "0.2-7037"
 $Env:CRAVE_POSTFIX = ".zip"
 if ($IsWindows -or $ENV:OS) {
     Write-Host "Downloading $Env:CRAVE_VERSION for Windows"
